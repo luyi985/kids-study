b6b522c2785880b971c32e6b0073d89c58b01c4d
Yi Lu <luyi985@gmail.com>
2023-01-05 14:14:18 +1100

    chore: basic clean

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: '',
   reactStrictMode: true,
-  exportPathMap: true,
-  images: {
-    unoptimized: true,
-  },
 }
 
 module.exports = nextConfig
